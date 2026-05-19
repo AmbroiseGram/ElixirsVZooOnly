9d4e8314767f10be9be1205a36eb57df4e6979fd
AmbroiseGram <114389417+AmbroiseGram@users.noreply.github.com>
2026-05-19 22:47:20 +0200

    order

--- Assets/Scripts/GameEngine/OrderManager.cs
+++ Assets/Scripts/GameEngine/OrderManager.cs
@@ -8,17 +8,52 @@ public class OrderManager : MonoBehaviour
     [SerializeField] GameObject orderPrefab;
     [SerializeField] Transform orderParent;
     [SerializeField] int startNumberOfOrders;
-    [SerializeField] float timeBetweenOrders;
-    float currentTimeBetweenOrders;
+
+    [Header("Configuration du Rythme")]
+    [SerializeField] private float timeBetweenOrders = 8f;   // Temps moyen souhaité pour 1 commande
+    [SerializeField] private int targetOrderCount = 3;        // Le nombre de commandes idéal visé
+    [SerializeField] private int maxOrderCount = 5;           // NOMBRE MAX : Limite physique de l'écran
+    [SerializeField] private float minSpawnInterval = 3f;     // Temps de respiration minimum entre 2 spawns
+    [SerializeField] private float maxSpawnInterval = 20f;    // Limite max de rythme si le joueur galère
+
+    private float spawnProgress;
+    private float timeSinceLastSpawn;
+
     public static OrderManager instance;
     [SerializeField] Level level;
+
     private void Awake()
     {
         instance = this;
     }
+
     private void Start()
     {
         for (int i = 0; i < startNumberOfOrders; i++)
+        {
+            CreateOrder();
+        }
+    }
+
+    private void Update()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+
+        int currentOrders = orderParent.childCount;
+
+        if (currentOrders >= maxOrderCount)
+        {
+            return;
+        }
+
+        float difference = currentOrders - targetOrderCount;
+        float orderRatio = (float)currentOrders / maxOrderCount;
+
+        float currentTargetDuration = Mathf.Lerp(minSpawnInterval, maxSpawnInterval, orderRatio);
+
+        spawnProgress += Time.deltaTime / currentTargetDuration;
+
+        if (spawnProgress >= 1f && timeSinceLastSpawn >= minSpawnInterval)
         {
             CreateOrder();
         }
@@ -28,26 +63,22 @@ public class OrderManager : MonoBehaviour
     {
         GameObject order = Instantiate(orderPrefab, orderParent);
         Order cOrder = order.GetComponent<Order>();
-        cOrder.Setup(OrderValueGenerator.instance.GetRandomValue(level.GetRandomDifficulty()));
-        currentTimeBetweenOrders = 0;
-    }
+        int diff = level.GetRandomDifficulty();
+        cOrder.Setup(OrderValueGenerator.instance.GetRandomValue(diff), diff);
 
-    private void Update()
-    {
-        currentTimeBetweenOrders += Time.deltaTime;
-        if (currentTimeBetweenOrders >= timeBetweenOrders)
-        {
-            CreateOrder();
-        }
+        spawnProgress = 0f;
+        timeSinceLastSpawn = 0f;
     }
 
     public void TrySolve(Value value)
     {
-        List<Order> orders = FindObjectsByType<Order>(FindObjectsSortMode.None).ToList();
+        List<Order> orders = orderParent.GetComponentsInChildren<Order>().ToList();
+
         var resultat = orders
-            .Where(x => x.value.value == value.value)
-            .OrderBy(x => 0-x.currentTimeBeforeExpire)
+            .Where(x => x.GetValue().value == value.value)
+            .OrderBy(x => 0 - x.GetTimeBeforeExpire())
             .FirstOrDefault();
+
         if (resultat != null)
         {
             Destroy(resultat.gameObject);
--- Assets/Scripts/GameplayElements/Order.cs
+++ Assets/Scripts/GameplayElements/Order.cs
@@ -1,31 +1,48 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Order : MonoBehaviour
 {
-    public Value value;
-    [SerializeField] float timeBeforeExpire;
-    public float currentTimeBeforeExpire;
+    Value value;
+    float timeBeforeExpire;
+    float currentTimeBeforeExpire;
     [SerializeField] TMP_Text valueText;
     bool isSetup = false;
     [SerializeField] Image timeBar;
-
-    public void Setup(Value value)
+    [SerializeField] List<float> timeBeforeExpireByLevels;
+    int level;
+    [SerializeField] Image orderImageDiff;
+    [SerializeField] List<Sprite> spritesByLevels;
+    public void Setup(Value value, int level)
     {
         this.value = value;
         isSetup = true;
+        this.level = level;
         currentTimeBeforeExpire = 0;
+        timeBeforeExpire = timeBeforeExpireByLevels[level];
         valueText.text = value.value.ToString();
+        orderImageDiff.sprite = spritesByLevels[level];
     }
     public void Update()
     {
         if (!isSetup) return;
         currentTimeBeforeExpire += Time.deltaTime;
-        timeBar.fillAmount = 1 - (currentTimeBeforeExpire / timeBeforeExpire);
+        timeBar.fillAmount = 1 - GetTimeBeforeExpire();
         if (currentTimeBeforeExpire >= timeBeforeExpire)
         {
             Destroy(gameObject);
         }
+    }
+
+    public Value GetValue()
+    {
+        return value;
+    }
+
+    public float GetTimeBeforeExpire()
+    {
+        return currentTimeBeforeExpire / timeBeforeExpire;
     }
 }
